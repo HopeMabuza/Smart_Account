8ff5909df3498a4dedf3b98bdc755247c147ced7
champagne101 <99389926+champagne101@users.noreply.github.com>
2026-03-25 13:12:20 +0200

    Update Test.sol

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -1,5 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.9;
+
+
+import "@account-abstraction/contracts/core/EntryPoint.sol";
+import "@account-abstraction/contracts/interfaces/IAccount.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/Create2.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
+
+
+contract Test {
+    constructor (bytes memory sig){
+    address recovered = ECDSA.recover(ECDSA.toEthSignedMessageHash(keccak256("hello")), sig);
+    console.log(recovered);
+
+    }
+
+}
 
 contract TestTarget {
     uint256 public x;
