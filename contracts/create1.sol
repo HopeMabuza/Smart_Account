62c9a4c7823c3d8dd575d9eafb86ceb626e73bc6
champagne101 <99389926+champagne101@users.noreply.github.com>
2026-03-25 13:10:53 +0200

    Update create1.sol

--- contracts/create1.sol
+++ contracts/create1.sol
@@ -1,1 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@account-abstraction/contracts/core/EntryPoint.sol";
+import "@account-abstraction/contracts/interfaces/IAccount.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/Create2.sol";
+
+contract Account is IAccount {
+    uint256 public count;
+    address public owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256)
+        external
+        view
+        returns (uint256 validationData)
+    {
+        address recovered = ECDSA.recover(ECDSA.toEthSignedMessageHash(userOpHash), userOp.signature);
+
+        return owner == recovered ? 0 : 1;
+    }
+
+    function execute() external {
+        count++;
+    }
+}
+
+
+
+
+contract AccountFactory {
+    function createAccount(address owner) external returns (address){
+       Account acc = new Account(owner);
+       return address(acc); 
+    } 
+}
+
+
 
