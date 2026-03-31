c77c8dc6709e5067a8b7b60a80fed59b21c955a1
wtc <londiwemabuza94@gmail.com>
2026-03-31 16:17:52 +0200

    managed to get signers address for UserOp signature field

--- scripts/signature.js
+++ scripts/signature.js
@@ -1,6 +1,27 @@
 const{ethers} = require("hardhat");
 
 async function main(){
+    //get signers
+    const [user1, user2] = await ethers.getSigners();
+    const user1Address = await user1.getAddress();
+    console.log("User1 address: ", user1Address);
+
+    //get message and hash it
+    const hashedMessage = ethers.keccak256(ethers.toUtf8Bytes("hello"));
+    console.log("Hashed message: ", hashedMessage);
+
+    //sign hashed message
+    const signature = await user1.signMessage(ethers.getBytes(hashedMessage));
+    console.log("Signature: ", signature);
+
+    //deploy Test contract and verify
+    const Test = await ethers.getContractFactory("Test");
+    const test = await Test.deploy(signature);
+    await test.waitForDeployment();
+
+    //get signers address
+    console.log("Signer's address: ", test.runner.address);
+    
 
 }
 main().catch(console.error)
