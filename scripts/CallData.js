849dc2bb8de72a8734356b3c09a92c1f959753c7
wtc <londiwemabuza94@gmail.com>
2026-03-31 17:18:42 +0200

    finished code in CallData.js

--- scripts/CallData.js
+++ scripts/CallData.js
@@ -1,6 +1,15 @@
 const {ethers} = require("hardhat");
 
 async function main(){
+    //get signers
+    const [user1, user2] = await ethers.getSigners();
+
+    //get Factory of Account
+    const Account = await ethers.getContractFactory("contracts/Account.sol:Account");
+
+    //get callData
+    const callData = await Account.interface.encodeFunctionData("execute", []);
+    console.log("CallData: ", callData);
 
 }
 main().catch(console.error)
