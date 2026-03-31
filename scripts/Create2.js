7322c3a57cfd9a43f428a63cc8eb308488f5ffc9
wtc <londiwemabuza94@gmail.com>
2026-03-31 16:22:31 +0200

    added comments in Create2.js

--- scripts/Create2.js
+++ scripts/Create2.js
@@ -1,6 +1,13 @@
 const {ethers} = require("hardhat");
 
 async function main(){
+    //get signers
+    //deploy AccountFactory
+    //call createAccount
+    //it returns the initial initCode
+    //add factory address with initcode to get actual initCode
+    //deploy EntryPoint
+    //use try catch of EntryPoint to get sender address
 
 }
 main().catch(console.error)
